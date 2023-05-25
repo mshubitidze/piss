7996e3f7205ef06c842f501ba40cca4ce843ea31
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2023-05-25 12:38:18 +0400

    piss

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col space-y-6 items-center justify-center">
-      <h1 className="text-4xl">Piss</h1>
+    <main className="flex min-h-screen mx-6 md:m-0 gap-2 flex-col items-center justify-center">
+      <h1 className="text-4xl text-yellow-500">Piss</h1>
       <Image
-        className="w-5/6 rounded-md"
+        className="rounded-md w-full md:w-auto"
         src="https://uploadthing.com/f/05544578-e88c-4c51-9d10-4c9cf9e3e754_piss.jpg"
         alt="piss"
         width={528}
