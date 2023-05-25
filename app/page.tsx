5b1ca50a16584f1092e612c5bc04b81fa3e81244
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2023-05-25 12:41:01 +0400

    piss

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen mx-6 md:m-0 gap-2 flex-col items-center justify-center">
       <h1 className="text-4xl text-yellow-500">Piss</h1>
       <Image
-        className="rounded-md w-full md:w-auto"
+        className="rounded-md w-full md:max-w-md"
         src="https://uploadthing.com/f/05544578-e88c-4c51-9d10-4c9cf9e3e754_piss.jpg"
         alt="piss"
         width={528}
