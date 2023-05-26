062f3f346370d662a5dbc1c9d27b12ab3911487d
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2023-05-26 18:53:09 +0400

    piss

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { buttonVariants } from "@/components/ui/button"
 
 export default function IndexPage() {
   return (
-    <section className="container flex my-44 flex-col items-center gap-6 pb-8 pt-6 md:py-10">
+    <section className="container my-44 flex flex-col items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-5xl font-extrabold leading-tight tracking-tighter md:text-6xl">
           Piss
